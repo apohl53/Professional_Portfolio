612e832722512f566ba5cf5ea3bef4709d1da613
alex pohlman <apohlman53@gmail.com>
2023-11-20 00:07:18 -0500

    brought in fontaweesome

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faDiscord } from "@fortawesome/free-brands-svg-icons";
+
 function Footer() {
   const date = new Date();
 
@@ -9,6 +13,23 @@ function Footer() {
       </p>
       <p>{date.getFullYear()}</p>
       <p>Developed by Alex Pohlman</p>
+      <a
+        href="https://www.linkedin.com/in/alex-pohlman-85765329b/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-dark me-3"
+      >
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+
+      <a
+        href="https://discordapp.com/users/1146083711591391383"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-dark"
+      >
+        <FontAwesomeIcon icon={faDiscord} />
+      </a>
     </footer>
   );
 }

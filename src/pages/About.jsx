0dd52ba8aca0611293a666b4d9e6135a47b685fc
alex pohlman <apohlman53@gmail.com>
2023-11-21 02:13:08 -0500

    ready to deploy

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import "../styles/aboutme.scss";
 
 function About() {
-  const divStyle = {};
+  const divStyle = {
+    textAlign: "center",
+    fontStyle: "italic",
+    marginTop: "20px",
+  };
 
   return (
     <>
-      <div id="#About" className="hero" style={divStyle}>
+      <div id="#About" className="hero">
         <div className="image-container">
           <img src="assets/photo.JPG" alt="Picture of Me" />
         </div>
-        <p>
+        <p style={divStyle}>
           My name is Alex Pohlman and Im an aspiring web developer. After
           working in entertainment an AV for almost a decade I decided to make
           the switch to the software side of things!{" "}
